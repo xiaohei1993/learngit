04aacbe80e3ebb547610bccc00a40a51aef879d8
xiaohei1993 <792959574@qq.com>
2015-10-08 13:36:44 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+	define('APP_NAME', 'Index');//前台项目名称
+	define('APP_PATH','./Index/');//项目路径
+	define('APP_DEBUG',TRUE);// 开启thinkphp调试模式,有助于我们书写查看错误。
+	require './ThinkPHP/ThinkPHP.php';//引入核心文件
+	echo "这里是前台"
+ ?>
